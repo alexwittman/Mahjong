0c9a2be03d8f74ba7545254dd989e1251d740f65
wittmanalex <wittmanalex@gmail.com>
2019-10-07 09:38:15 -0500

    Added structure for additional action testing.

---
+++ test/ron_test.js
@@ -0,0 +1,13 @@
+let Player = require('../src/game/player').Player;
+let TileList = require('../src/game/tile').TileList;
+let Meld = require('../src/game/meld').Meld;
+let Tile = require('../src/game/tile').Tile;
+let Hand = require('../src/game/hand').Hand;
+let expect = require('chai').expect;
+
+describe('Ron', () => {
+
+    it('', () => {
+
+    });
+});
