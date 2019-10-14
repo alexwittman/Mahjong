9b62c097216615c56206477bc87a3ac2d47582eb
wittmanalex <wittmanalex@gmail.com>
2019-10-14 18:04:31 -0500

    Added some canRon tests.

--- test/ron_test.js
+++ test/ron_test.js
@@ -8,23 +8,48 @@ let expect = require('chai').expect;
 describe('Ron', () => {
 
     it('A player can ron if the tile discarded makes their hand complete with >= 1 han.', () => {
-
+        let player = new Player(0);
+        let tiles = TileList('p1122233344455');
+        let melds = [];
+        let availableTile = TileList('p1')[0];
+        player.hand = new Hand(tiles, melds);
+        expect(player.CanRon(availableTile)).to.eql(true);
     });
 
     it('A player cannot ron if the tile discarded makes their hand complete with < 1 han.', () => {
-
+        let player = new Player(0);
+        let tiles = TileList('p11333444a55');
+        let melds = [new Meld(TileList('p123'))];
+        let availableTile = TileList('p1')[0];
+        player.hand = new Hand(tiles, melds);
+        expect(player.CanRon(availableTile)).to.eql(false);
     });
 
     it('A player cannot ron if the tile discarded does not complete their hand.', () => {
-
+        let player = new Player(0);
+        let tiles = TileList('p11333444a55');
+        let melds = [new Meld(TileList('p123'))];
+        let availableTile = TileList('E')[0];
+        player.hand = new Hand(tiles, melds);
+        expect(player.CanRon(availableTile)).to.eql(false);
     });
 
     it('A player can ron if the tile discarded makes their hand complete with a pong.', () => {
-
+        let player = new Player(0);
+        let tiles = TileList('p11333444a55');
+        let melds = [new Meld(TileList('p555'))];
+        let availableTile = TileList('p1')[0];
+        player.hand = new Hand(tiles, melds);
+        expect(player.CanRon(availableTile)).to.eql(true);
     });
 
     it('A player can ron if the tile discarded makes their hand complete with a chow.', () => {
-
+        let player = new Player(0);
+        let tiles = TileList('p12s123a123555EE');
+        let melds = [];
+        let availableTile = TileList('p3')[0];
+        player.hand = new Hand(tiles, melds);
+        expect(player.CanRon(availableTile)).to.eql(true);
     });
 
     it('A player can ron if the tile discarded makes their hand complete with a pair.', () => {
