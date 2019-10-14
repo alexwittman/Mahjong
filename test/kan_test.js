c718e3de52fc93ee625e1bea7858fe5499447b5b
wittmanalex <wittmanalex@gmail.com>
2019-10-14 11:17:48 -0500

    Added kan tests.

--- test/kan_test.js
+++ test/kan_test.js
@@ -16,13 +16,23 @@ describe('Kan', () => {
         expect(player.CanKan(availableTile)).to.eql(true);
     });
 
-    it('A player cannot kan with two tiles the same as the tile discarded.', () => {
+    it('A player can kan with open pong and have fourth tile in hand.', () => {
         let player = new Player(0);
-        let tiles = TileList('p1122233344455');
+        let tiles = TileList('p1112223334445');
         let melds = [];
         let availableTile = TileList('p1')[0];
         player.hand = new Hand(tiles, melds);
-        expect(player.CanKan(availableTile)).to.eql(false);
+        expect(player.CanKan(availableTile)).to.eql(true);
+    });
+
+    it('A player cannot kan with two tiles the same as the tile discarded.', () => {
+        let player = new Player(0);
+        let tiles = TileList('p1222333444');
+        let melds = [new Meld(TileList('p111'), true)];
+        let drawnTile = TileList('E')[0];
+        player._drawnTile = drawnTile;
+        player.hand = new Hand(tiles, melds);
+        expect(player.CanKan()).to.eql(true);
     });
 
     it('A player can kan with an open pong and draw the fourth tile.', () => {
@@ -67,5 +77,70 @@ describe('Kan', () => {
         let melds = [];
         player.hand = new Hand(tiles, melds);
         expect(player.CanKan()).to.eql(true);
+    });
+
+    it('KanMelds returns the correct value for a player who can form one closed kong with closed tiles.', () => {
+        let player = new Player(0);
+        let tiles = TileList('p1111222333444');
+        let melds = [];
+        player.hand = new Hand(tiles, melds);
+        let kongs = [new Meld(TileList('p1111'))];
+        expect(player.KanMelds(null)).to.eql(kongs);
+    });
+
+    it('KanMelds returns the correct value for a player who can form one closed kong from drawn tile.', () => {
+        let player = new Player(0);
+        let tiles = TileList('p1112223334445');
+        let melds = [];
+        let drawnTile = TileList('p1')[0];
+        player._drawnTile = drawnTile;
+        player.hand = new Hand(tiles, melds);
+        let kongs = [new Meld(TileList('p1111'))];
+        expect(player.KanMelds(null)).to.eql(kongs);
+    });
+
+    it('KanMelds returns the correct value for a player who can form one closed kong from other\'s discard.', () => {
+        let player = new Player(0);
+        let tiles = TileList('p111222333444');
+        let melds = [];
+        let availableTile = TileList('p1')[0];
+        let drawnTile = null;
+        player._drawnTile = drawnTile;
+        player.hand = new Hand(tiles, melds);
+        let kongs = [new Meld(TileList('p1111'), true)];
+        expect(player.KanMelds(availableTile)).to.eql(kongs);
+    });
+
+    it('KanMelds returns the correct value for a player who can form two kongs.', () => {
+        let player = new Player(0);
+        let tiles = TileList('p111222233444');
+        let melds = [];
+        let drawnTile = TileList('p1')[0];
+        player._drawnTile = drawnTile;
+        player.hand = new Hand(tiles, melds);
+        let kongs = [new Meld(TileList('p1111')), new Meld(TileList('p2222'))];
+        expect(player.KanMelds(null)).to.eql(kongs);
+    });
+
+    it('KanMelds returns the correct value for a player who can form three kongs.', () => {
+        let player = new Player(0);
+        let tiles = TileList('p1112222334');
+        let melds = [new Meld(TileList('p444'))];
+        let drawnTile = TileList('p1')[0];
+        player._drawnTile = drawnTile;
+        player.hand = new Hand(tiles, melds);
+        let kongs = [new Meld(TileList('p1111')), new Meld(TileList('p2222')), new Meld(TileList('p4444'))];
+        expect(player.KanMelds(null)).to.eql(kongs);
+    });
+
+    it('KanMelds returns the correct value for a player who can form no kongs.', () => {
+        let player = new Player(0);
+        let tiles = TileList('p1112223334445');
+        let melds = [];
+        let drawnTile = TileList('E')[0];
+        player._drawnTile = drawnTile;
+        player.hand = new Hand(tiles, melds);
+        let kongs = [];
+        expect(player.KanMelds(null)).to.eql(kongs);
     });
 });
