1e31f6a2d948d1075211800b6bf3cdef80049913
wittmanalex <wittmanalex@gmail.com>
2019-10-15 14:05:00 -0500

    New class to handle calculating value of complete hands.

---
+++ src/game/value_calculator.js
@@ -0,0 +1,33 @@
+let yaku = require('./yaku');
+
+/**
+ * Class to calculate the value of a winning hand.
+ */
+class Value_Calculator {
+
+    /**
+     * Calculates the total han of a list of yaku.
+     * 
+     * @param {yaku.Yaku[]} yakuList The list of yaku to evaluate.
+     * @param {boolean} handOpen True if the hand is open, false otherwise.
+     * @returns {number} The total han value of all yaku in the list.
+     */
+    CalculateHan(yakuList, handOpen){
+        let han = 0;
+        for(let yaku of yakuList){
+            if(handOpen){
+                if(!isNaN(yaku.hanOpen)){
+                    han += yaku.hanOpen;
+                }
+            }
+            else{
+                if(!isNaN(yaku.hanClosed)){
+                    han += yaku.hanClosed;
+                }
+            }
+        }
+        return han;
+    }
+}
+
+module.exports.Value_Calculator = Value_Calculator;
