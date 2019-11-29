c11e2e1c06ad35ea845681c0f839df425c7dac42
wittmanalex <wittmanalex@gmail.com>
2019-11-29 15:45:28 -0600

    Adpated tests to work with new function implementaton.

--- test/chi_test.js
+++ test/chi_test.js
@@ -3,45 +3,56 @@ let TileList = require('../src/game/tile').TileList;
 let Meld = require('../src/game/meld').Meld;
 let Tile = require('../src/game/tile').Tile;
 let Hand = require('../src/game/hand').Hand;
+let Discard = require('../src/game/discard').Discard;
 let expect = require('chai').expect;
 let TileListCount = require('../src/game/tile').TileListCount;
 
 describe('Chi', () => {
 
     it('A player can chi with one potential chow.', () => {
-        let player = new Player(0);
+        let player = new Player(3);
         let tiles = TileList('p12s111222a666NN');
         let melds = [];
-        let availableTile = TileList('p3')[0];
+        let availableDiscard = new Discard(TileList('p3')[0], 2);
         player.hand = new Hand(tiles, melds);
-        expect(player.CanChi(availableTile)).to.eql(true);
+        expect(player.CanChi(availableDiscard)).to.eql(true);
     });
 
     it('A player can chi with multiple potential chows.', () => {
-        let player = new Player(0);
+        let player = new Player(1);
         let tiles = TileList('p1245s111222a666');
         let melds = [];
-        let availableTile = TileList('p3')[0];
+        let availableDiscard = new Discard(TileList('p3')[0], 0);
         player.hand = new Hand(tiles, melds);
-        expect(player.CanChi(availableTile)).to.eql(true);
+        expect(player.CanChi(availableDiscard)).to.eql(true);
     });
 
     it('A player cannot chi with zero potential chows.', () => {
         let player = new Player(0);
         let tiles = TileList('p13s111222a666NN');
         let melds = [];
-        let availableTile = TileList('p3')[0];
+        let availableDiscard = new Discard(TileList('p3')[0], 3);
         player.hand = new Hand(tiles, melds);
-        expect(player.CanChi(availableTile)).to.eql(false);
+        expect(player.CanChi(availableDiscard)).to.eql(false);
     });
 
-    // it('A player can chi only from the person to the left of them.', () => {
+    it('A player can chi only from the person to the left of them.', () => {
+        let player = new Player(3);
+        let tiles = TileList('p13s111222a666NN');
+        let melds = [];
+        let availableDiscard = new Discard(TileList('p2')[0], 2);
+        player.hand = new Hand(tiles, melds);
+        expect(player.CanChi(availableDiscard)).to.eql(true);
+    });
 
-    // });
-
-    // it('A player cannot chi from a person not to the left of them.', () => {
-
-    // });
+    it('A player cannot chi from a person not to the left of them.', () => {
+        let player = new Player(0);
+        let tiles = TileList('p13s111222a666NN');
+        let melds = [];
+        let availableDiscard = new Discard(TileList('p2')[0], 1);
+        player.hand = new Hand(tiles, melds);
+        expect(player.CanChi(availableDiscard)).to.eql(false);
+    });
 
     it('When a player chi\'s the hand now contains a meld with the chow.', () => {
         let player = new Player(0);
