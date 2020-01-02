5fd4489a30fc79e37e60f5c8b10b65bc8108713e
wittmanalex <wittmanalex@gmail.com>
2020-01-02 14:16:47 -0500

    Modified players to incorporate new ai class.

--- src/game/game.js
+++ src/game/game.js
@@ -3,22 +3,25 @@
 let Round = require('./round').Round;
 let Player = require('./player').Player;
 let CONST = require('./constants');
+let AI = require('./ai').AI;
 
 /**
  * Class to hold an instance of a mahjong game.
  */
 class _Game {
-
     /**
      * Constructor for Game class.
-     * 
+     *
      * Creates 4 players.
+     *
+     * @param {number} length The number of winds to go through for the game, 1-4.
      */
     constructor(length) {
         this._players = [];
-        for (let i = 0; i < 4; i++) {
-            this._players.push(new Player(i));
-        }
+        this._players.push(new Player(0, CONST.EAST, new AI()));
+        this._players.push(new Player(1, CONST.SOUTH, new AI()));
+        this._players.push(new Player(2, CONST.WEST, new AI()));
+        this._players.push(new Player(3, CONST.NORTH, new AI()));
         this._length = length;
     }
 
@@ -26,7 +29,7 @@ class _Game {
      * Begins a game by starting the first round.
      */
     StartGame() {
-        console.log("Game Started.")
+        console.log('Game Started.');
         let roundWind = CONST.EAST;
         for (let i = 0; i < this._length; i++, roundWind++) {
             for (let i = 1; i <= 4; i++) {
@@ -39,7 +42,7 @@ class _Game {
      * Starts a round of the game.
      */
     StartRound(roundNumber, roundWind) {
-        console.log("Round " + roundNumber + " Started.")
+        console.log('Round ' + roundNumber + ' Started.');
         let round = new Round(this._players, roundNumber, roundWind);
         round.StartRound();
     }
@@ -47,4 +50,4 @@ class _Game {
 
 module.exports = {
     Game: _Game
-}
+};
