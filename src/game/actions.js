4d68268f186ae22541630908b0192af5ea304dcd
wittmanalex <wittmanalex@gmail.com>
2020-01-20 21:27:49 -0600

    Added string to action function and able to function.

--- src/game/actions.js
+++ src/game/actions.js
@@ -2,10 +2,9 @@
  * Host class for an action type.
  */
 class Action {
-
     /**
      * Constructor for Action class.
-     * 
+     *
      * @param {ActionType} type The type of action to be represented by this class.
      */
     constructor(type) {
@@ -14,7 +13,7 @@ class Action {
 
     /**
      * Getter method for Action class.
-     * 
+     *
      * @returns {ActionType} The type of action.
      */
     get type() {
@@ -32,10 +31,45 @@ let ActionType = {
     Kan: 3,
     Riichi: 4,
     Ron: 5,
-    Tsumo: 6
+    Tsumo: 6,
+    None: 7
+};
+
+/**
+ * Converts string input to actiontype.
+ * @param {string} action String of the action.
+ */
+function StringToAction(action) {
+    switch (action) {
+        case '0':
+            return ActionType.Discard;
+        case '1':
+            return ActionType.Chi;
+        case '2':
+            return ActionType.Pon;
+        case '3':
+            return ActionType.Kan;
+        case '4':
+            return ActionType.Riichi;
+        case '5':
+            return ActionType.Ron;
+        case '6':
+            return ActionType.Tsumo;
+        default:
+            return ActionType.None;
+    }
+}
+
+function AbleTo(actions, actionToCheck) {
+    for (let action of actions) {
+        if (action == actionToCheck) return true;
+    }
+    return false;
 }
 
 module.exports = {
     Action: Action,
-    ActionType: ActionType
-}
+    ActionType: ActionType,
+    StringToAction: StringToAction,
+    AbleTo: AbleTo
+};
